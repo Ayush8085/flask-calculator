d6c6ea59ca58263c2152bf9a045cba21324b12c3
Ayush <ayushkumar9601@gmail.com>
2023-08-08 17:06:45 +0530

    api modified

--- app.py
+++ app.py
@@ -4,117 +4,32 @@ import sqlite3
 app = Flask(__name__)
 
 # Creating sqlite
-conn = sqlite3.connect('cal.sqlite')
+conn = sqlite3.connect('cal1.sqlite')
 cur = conn.cursor()
 
 cur.execute('''
     CREATE TABLE IF NOT EXISTS calculations (
         id INTEGER PRIMARY KEY,
         operation TEXT NOT NULL,
-        num1 REAL NOT NULL,
-        num2 REAL NOT NULL,
+        expression TEXT NOT NULL,
         result REAL NOT NULL
     )
 ''')
 conn.commit()
 
 # Endpoint for addition
-@app.route('/add', methods=['POST'])
-def add():
+@app.route('/api', methods=['POST'])
+def solve():
     data = request.json
-    if 'num1' not in data or 'num2' not in data:
-        return jsonify({'error': 'Invalid input'}), 400
 
-    result = data['num1'] + data['num2']
+    result = eval(data['expression'])
 
     # Save in sqlite
-    conn = sqlite3.connect('cal.sqlite')
+    conn = sqlite3.connect('cal1.sqlite')
     cur = conn.cursor()
 
-    cur.execute('INSERT INTO calculations (operation, num1, num2, result) VALUES (?, ?, ?, ?)',
-              ('add', data['num1'], data['num2'], result))
-    conn.commit()
-
-    return jsonify({'result': result})
-
-# Endpoint for subtraction
-@app.route('/subtract', methods=['POST'])
-def subtract():
-    data = request.json
-    if 'num1' not in data or 'num2' not in data:
-        return jsonify({'error': 'Invalid input'}), 400
-
-    result = data['num1'] - data['num2']
-
-    # Save in sqlite
-    conn = sqlite3.connect('cal.sqlite')
-    cur = conn.cursor()
-
-    cur.execute('INSERT INTO calculations (operation, num1, num2, result) VALUES (?, ?, ?, ?)',
-              ('subtract', data['num1'], data['num2'], result))
-    conn.commit()
-    
-    return jsonify({'result': result})
-
-# Endpoint for multiplication
-@app.route('/multiply', methods=['POST'])
-def multiply():
-    data = request.json
-    if 'num1' not in data or 'num2' not in data:
-        return jsonify({'error': 'Invalid input'}), 400
-
-    result = data['num1'] * data['num2']
-
-    # Save in sqlite
-    conn = sqlite3.connect('cal.sqlite')
-    cur = conn.cursor()
-
-    cur.execute('INSERT INTO calculations (operation, num1, num2, result) VALUES (?, ?, ?, ?)',
-              ('multiply', data['num1'], data['num2'], result))
-    conn.commit()
-    
-    return jsonify({'result': result})
-
-# Endpoint for division
-@app.route('/divide', methods=['POST'])
-def divide():
-    data = request.json
-    if 'num1' not in data or 'num2' not in data:
-        return jsonify({'error': 'Invalid input'}), 400
-
-    if data['num2'] == 0:
-        return jsonify({'error': 'Division by zero'}), 400
-
-    result = data['num1'] / data['num2']
-
-    # Save in sqlite
-    conn = sqlite3.connect('cal.sqlite')
-    cur = conn.cursor()
-
-    cur.execute('INSERT INTO calculations (operation, num1, num2, result) VALUES (?, ?, ?, ?)',
-              ('divide', data['num1'], data['num2'], result))
-    conn.commit()
-    
-    return jsonify({'result': result})
-
-# Endpoint for division
-@app.route('/modulo', methods=['POST'])
-def modulo():
-    data = request.json
-    if 'num1' not in data or 'num2' not in data:
-        return jsonify({'error': 'Invalid input'}), 400
-
-    if data['num2'] == 0:
-        return jsonify({'error': 'Division by zero'}), 400
-
-    result = data['num1'] % data['num2']
-
-    # Save in sqlite
-    conn = sqlite3.connect('cal.sqlite')
-    cur = conn.cursor()
-
-    cur.execute('INSERT INTO calculations (operation, num1, num2, result) VALUES (?, ?, ?, ?)',
-              ('modulo', data['num1'], data['num2'], result))
+    cur.execute('INSERT INTO calculations (operation, expression, result) VALUES (?, ?, ?)',
+              ('add', data['expression'], result))
     conn.commit()
 
     return jsonify({'result': result})
